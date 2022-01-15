9b2c65344e8309aca5f92c1322219e818c3345bd
Jason <Jaytonbye@yahoo.com>
2022-01-14 19:38:08 -0500

    no change

--- src/server/middleware/passport-stradegies.ts
+++ src/server/middleware/passport-stradegies.ts
@@ -1,46 +1,55 @@
-import config from '../config'
+import config from "../config";
 import blogs from "../db/blogs";
-import * as passport from 'passport';
-import * as PassportLocal from 'passport-local';
-import * as PassportJWT from 'passport-jwt';
+import * as passport from "passport";
+import * as PassportLocal from "passport-local";
+import * as PassportJWT from "passport-jwt";
 import { compareHash } from "../utils";
-import { Application } from 'express';
-
+import { Application } from "express";
 
 export function configurePassport(app: Application) {
-    passport.serializeUser((user: any /*I suck*/, done) => {
-        if (user.password) {
-            delete user.password;
-        } done(null, user);
-    });
+  passport.serializeUser((user: any /*I suck*/, done) => {
+    if (user.password) {
+      delete user.password;
+    }
+    done(null, user);
+  });
 
-    passport.deserializeUser((user, done) => done(null, user));
+  passport.deserializeUser((user, done) => done(null, user));
 
-    passport.use(new PassportLocal.Strategy({
-        usernameField: 'email'
-    }, async (email, password, done) => {
+  passport.use(
+    new PassportLocal.Strategy(
+      {
+        usernameField: "email",
+      },
+      async (email, password, done) => {
         try {
-            const [userFound] = await blogs.find("email", email);
-            if (userFound && compareHash(password, userFound.password)) {
-                done(null, userFound);
-            } else {
-                done(null, false);
-            }
+          const [userFound] = await blogs.find("email", email);
+          if (userFound && compareHash(password, userFound.password)) {
+            done(null, userFound);
+          } else {
+            done(null, false);
+          }
         } catch (error) {
-            done(error);
+          done(error);
         }
-    }))
+      }
+    )
+  );
 
-    passport.use(new PassportJWT.Strategy({
+  passport.use(
+    new PassportJWT.Strategy(
+      {
         jwtFromRequest: PassportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
         secretOrKey: config.jwt.secret,
-    }, (payload: any, done) => {
+      },
+      (payload: any, done) => {
         try {
-            done(null, payload)
+          done(null, payload);
         } catch (error) {
-            done(error);
+          done(error);
         }
-    }))
-    app.use(passport.initialize());
+      }
+    )
+  );
+  app.use(passport.initialize());
 }
-
